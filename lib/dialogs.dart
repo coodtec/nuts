866e7a969c985d67d8530516c7c0a9440b6da6b7
coodtec <john@coodtec.com>
2022-11-18 14:08:34 -0500

    Update dialogs.dart

--- lib/dialogs.dart
+++ lib/dialogs.dart
@@ -5,27 +5,26 @@ class PopAlert {
   String title;
   String message;
   String actText;
-  void Function()? onPressed; 
+  void Function()? onPressed;
+  bool cancelButton;
 
-  PopAlert(
-      {required this.context, 
-      required this.message, 
-      this.title = 'Alert',
-      this.actText = 'OK',
-      this.onPressed,
-      }) {
+  PopAlert({
+    required this.context,
+    required this.message,
+    this.title = 'Alert',
+    this.actText = 'OK',
+    this.onPressed,
+    this.cancelButton = false,
+  }) {
     showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
         title: Text(title),
         content: Text(message),
         actions: <Widget>[
-/*           TextButton(
-            onPressed: () => Navigator.pop(context, 'Cancel'),
-            child: const Text('Cancel'),
-          ), */
+          CancelButton(cancel: true,context: context,),
           TextButton(
-            onPressed:  extendOnPressed(onPressed,context,actText),
+            onPressed: layedOnPressed(onPressed, context, actText),
             child: Text(actText),
           ),
         ],
@@ -35,11 +34,34 @@ class PopAlert {
 }
 
 
-Function()? extendOnPressed (Function()? onPressed, BuildContext context, String text){
+class CancelButton extends StatelessWidget {
+  const CancelButton({
+    Key? key,
+    required this.cancel,
+    required this.context,
+  }) : super(key: key);
+
+  final bool cancel;
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    if (cancel) {
+      return TextButton(
+        onPressed: () => Navigator.pop(context, 'Cancel'),
+        child: const Text('Cancel'),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
+
+Function()? layedOnPressed(
+    Function()? onPressed, BuildContext context, String text) {
   if (onPressed == null) {
     return () => Navigator.pop(context, 'OK');
-  }
-  else {
+  } else {
     return (() {
       onPressed;
       Navigator.pop(context, text);
