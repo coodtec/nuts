1197c44b5b328ad56adade7454cd16a302692273
coodtec <john@coodtec.com>
2022-11-24 07:49:29 -0500

    Update dialogs.dart

--- lib/src/dialogs.dart
+++ lib/src/dialogs.dart
@@ -1,5 +1,42 @@
 part of nuts;
 
+Future<String> showAlertDialog(
+    { required BuildContext context,
+      required String message,
+      String title = 'Alert',
+      String ok = 'Ok',
+      bool showCancel = false,
+      String cancel = 'Cancel',
+    }) async {
+  
+  // set up the buttons
+  Widget cancelButton = FlatButton(
+    child: Text(cancel),
+    onPressed: () => Navigator.pop(context, cancel),
+  );
+  Widget continueButton = FlatButton(
+    child: Text(ok),
+    onPressed: () => Navigator.pop(context, ok),
+  );
+      
+  return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          showCancel?  cancelButton:const SizedBox.shrink(),
+          continueButton,
+        ],
+      );
+    },
+  );
+}
+
+
+/* 
+
 class PopAlert {
   BuildContext context;
   String title;
@@ -26,7 +63,7 @@ class PopAlert {
     ).then((value) => value);
   }
 }
-
+ */
 
 /* class CancelButton extends StatelessWidget {
   const CancelButton({
